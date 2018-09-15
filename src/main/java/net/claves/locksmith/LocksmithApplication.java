d8eacb21240b3802ade6a55451c0f0a68d450dd4
focoma <focoma@gmail.com>
2018-09-15 16:16:51 +0800

    initial commit

---
+++ src/main/java/net/claves/locksmith/LocksmithApplication.java
@@ -0,0 +1,12 @@
+package net.claves.locksmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocksmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocksmithApplication.class, args);
+	}
+}
